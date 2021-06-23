b617134284a5f966b519743a49c6bcd89c1a5300
Zmart64 <marteneckardt@hotmail.com>
2021-06-23 16:13:25 +0200

    included test for exception that is thrown after wrongly formatted input

---
+++ test/WrongFormatTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import smaa_creation.AGG;
+
+import java.io.FileNotFoundException;
+
+public class WrongFormatTest {
+
+    //test if exception is thrown when calculation wrongly formatted agg
+    @Test(expected = Exception.class)
+    public void testWrongFormat() throws FileNotFoundException {
+        AGG test = new AGG("test/resources/scenario4_wrong_format.csv");
+    }
+}
