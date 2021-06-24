7fce899edb0650c205d6920a6e787a14d2b3651d
Zmart <marteneckardt@hotmail.com>
2021-06-24 12:26:14 +0200

    changed comment

--- test/WrongFormatTest.java
+++ test/WrongFormatTest.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 
 public class WrongFormatTest {
 
-    //test if exception is thrown when calculation wrongly formatted agg
+    //test if exception is thrown when calculation receives wrongly formatted agg
     @Test(expected = Exception.class)
     public void testWrongFormat() throws FileNotFoundException {
         AGG test = new AGG("test/resources/scenario4_wrong_format.csv");
